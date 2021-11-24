e5f971b0ce05c39ee8feadbceef4db786d40c11e
Camila de Cássia Silva <camila.silva@ges.inatel.br>
2021-11-24 13:37:42 -0300

    Revert "arquivos .java e .feature"
    This reverts commit 942a4eae0d77f757e7c0c885f1ae53f83c6380c9.

--- ListaAPI/aula_api/src/test/java/aula_inatel/S206TestRunner.java
+++ ListaAPI/aula_api/src/test/java/aula_inatel/S206TestRunner.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class S206TestRunner {
-  
+ 
     @Test
     void testParallel() {
         Results results = Runner.path("classpath:aula_inatel")
